abff16b022fd4ec59185b6e7aaa6fa975032309a
zjls6 <zjls6@qq.com>
2025-04-06 23:27:58 +0800

    import convex auth nextjs api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {ConvexClientProvider} from "@/components/ConvexClientProvider";
+import {ConvexAuthNextjsServerProvider} from "@convex-dev/auth/nextjs/server";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -20,12 +21,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-            <ConvexClientProvider>
-                {children}
-            </ConvexClientProvider>
-        </body>
-        </html>
+        <ConvexAuthNextjsServerProvider>
+            <html lang="en">
+                <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                    <ConvexClientProvider>
+                        {children}
+                    </ConvexClientProvider>
+                </body>
+            </html>
+        </ConvexAuthNextjsServerProvider>
     );
 }
