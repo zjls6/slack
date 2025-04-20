7585c6c650cec6dd5314820f1af2cc8274438180
zjls6 <zjls6@qq.com>
2025-04-20 22:11:21 +0800

    add workspaces table

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,10 +1,14 @@
-import { defineSchema,defineTable } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
 import { v } from "convex/values"
- 
+
 const schema = defineSchema({
-  ...authTables,
-  // Your other tables...
+    ...authTables,
+    workspaces: defineTable({
+        name: v.string(),
+        userId: v.id("users"),
+        joinCode: v.string()
+    })
 });
- 
+
 export default schema;
