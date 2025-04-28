e0525fc553ccc44f5ba322f844989d3956f16aea
zjls <zjls6@qq.com>
2025-04-28 10:22:08 +0800

    fix nest button error

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -4,7 +4,7 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { Button, ButtonDiv } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
@@ -27,15 +27,15 @@ export const WorkspaceSwitcher = () => {
     return (
         <div>
             <DropdownMenu>
-                <DropdownMenuTrigger>{/*nest button do not allowed*/}
-                    <ButtonDiv
+                <DropdownMenuTrigger asChild>{/*nest button do not allowed*/}
+                    <Button
                         className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
                         { workspaceLoading ? (
                             <Loader className="size-5 animate-spin shrink-0"/>
                         ) : (
                             workspace?.name.charAt(0).toUpperCase()
                         ) }
-                    </ButtonDiv>
+                    </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent side="bottom" align="start" className="w-64">
                     <DropdownMenuItem onClick={ () => router.push(`/workspace/${ workspaceId }`) }
